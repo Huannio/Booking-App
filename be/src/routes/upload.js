8e10f6e74bf15eccc82c3031bcb088adf968da7a
Huannio <2221050004@studentstudent.humg.edu.vn>
2025-03-03 16:58:24 +0700

    upload

--- be/src/routes/upload.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UploadController = require("../app/controllers/UploadController");
-const upload = require("../middleware/upload");
-// GET /upload
-router.post("/image", upload.array("images"), UploadController.uploadImage);
-
-module.exports = router;
